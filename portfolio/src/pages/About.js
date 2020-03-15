3587c12f66a98bb2430b564f5b63a21502a9345f
Cleveland Pitt <cleveland.pitt@gmail.com>
2020-03-15 01:39:07 -0400

    imported container row and column on about.js page

--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -1,4 +1,7 @@
 import React from "react"
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
  
 function About(props) {
   return (
